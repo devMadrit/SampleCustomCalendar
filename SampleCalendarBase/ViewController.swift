77208cc981ba26e4b2a20fe196a1131c2fbe3033
Madrit Kacabumi <madrit.kacabumi@gmail.com>
2019-02-02 20:27:11 +0100

    Initial Commit

---
+++ SampleCalendarBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleCalendarBase
+//
+//  Created by Madrit on 02/02/2019.
+//  Copyright © 2019 Madrit Kacabumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
